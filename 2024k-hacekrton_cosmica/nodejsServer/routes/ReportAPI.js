7be420ea9653a930903bf5de6f158bf9408c1123
yanghaemi <heau0105@naver.com>
2024-08-08 20:57:41 +0900

    [get] report list 불러오기 기능 추가

--- 2024k-hacekrton_cosmica/nodejsServer/routes/ReportAPI.js
+++ 2024k-hacekrton_cosmica/nodejsServer/routes/ReportAPI.js
@@ -7,13 +7,46 @@ var router = express.Router();
 var db = require('../models/index');
 
 
-router.post('/create', async (req, res) => {
-
+/**신고 접수 리스트를 불러오는 라우터
+ * 호출주소: http://localhost:5000/report/
+ */
+router.get('/', async (req, res) => {
     let apiResult = {
-        code : 500,
+        code : 400,
         data : "",
         msg : ""
     }
+    try {
+        
+        // DB내 신고 정보들 다 가져오기
+        var reports = await db.report.findAll();
+
+        apiResult.code = 200;
+        apiResult.data = reports;
+        apiResult.msg = "Ok";
+    } catch (err) {
+        console.log("에러: ",err);
+
+        apiResult.code = 500;
+        apiResult.data = null;
+        apiResult.msg = "Failed";
+    }
+
+    res.json(reports);
+});
+
+/**
+ * 신고 접수 라우터
+ * 호출주소 : http://localhost:5000/report/create
+ */
+router.post('/create', async (req, res) => {
+
+    let apiResult = {
+        code : 400,
+        data : "",
+        msg : ""
+    }
+    
     try{const report = {
         title: req.body.title,
         contents: req.body.contents,
