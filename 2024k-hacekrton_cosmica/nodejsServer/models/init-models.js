99ec126e644d5f5dd0eff7d918e34e04ba9d4e86
jeongseonwoong <tjsdnd1219@naver.com>
2024-09-09 22:21:16 +0900

    db 공유(?) 실행

--- 2024k-hacekrton_cosmica/nodejsServer/models/init-models.js
+++ 2024k-hacekrton_cosmica/nodejsServer/models/init-models.js
@@ -1,29 +1,9 @@
 var DataTypes = require("sequelize").DataTypes;
-var _appointment = require("./appointment");
-var _report = require("./report");
-var _user = require("./user");
-var _userappointment = require("./userappointment");
 
 function initModels(sequelize) {
-  var appointment = _appointment(sequelize, DataTypes);
-  var report = _report(sequelize, DataTypes);
-  var user = _user(sequelize, DataTypes);
-  var userappointment = _userappointment(sequelize, DataTypes);
 
-  userappointment.belongsTo(appointment, { as: "appointment", foreignKey: "appointmentId"});
-  appointment.hasMany(userappointment, { as: "userappointments", foreignKey: "appointmentId"});
-  appointment.belongsTo(user, { as: "wheelchair", foreignKey: "wheelchairId"});
-  user.hasMany(appointment, { as: "appointments", foreignKey: "wheelchairId"});
-  appointment.belongsTo(user, { as: "companion", foreignKey: "companionId"});
-  user.hasMany(appointment, { as: "companion_appointments", foreignKey: "companionId"});
-  userappointment.belongsTo(user, { as: "user", foreignKey: "userId"});
-  user.hasMany(userappointment, { as: "userappointments", foreignKey: "userId"});
 
   return {
-    appointment,
-    report,
-    user,
-    userappointment,
   };
 }
 module.exports = initModels;
